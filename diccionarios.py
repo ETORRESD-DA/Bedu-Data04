16e6122f5574fd3ef2f4a573d9cc06470a3eed80
ETORRESD <etorresdelucio@hotmail.com>
2020-08-29 11:27:17 -0500

    Carga ejercicios

---
+++ diccionarios.py
@@ -0,0 +1,31 @@
+data={
+    "students":[
+        {
+            'name':'Salvador Vizcaino',
+            'class':2015,
+            'scores':[10,9.8,7,7.8,10,8.9]
+        },
+        {
+            'name':'Edgar Torres',
+            'class':2015,
+            'scores':[10,7.8,9.9,8,10,8.9]
+        },
+        {
+            'name':'Galileo Guzman',
+            'class':2015,
+            'scores':[10,8,8,6.9,8.9,8.9]
+        },
+         {
+            'name':'Met Velasquez',
+            'class':2015,
+            'scores':[10,10,9.8,9.9,9.9,8.9]
+        },
+    ]
+}
+
+print(type(data))
+print(data)
+
+students=data.get('students',[])
+
+students=data['students']
